5684a90fa2fa9dd7540fd0a3324fe9e4ec69f010
baraka caleb <65470823+pimby@users.noreply.github.com>
2023-10-13 11:00:48 +0300

    Commited first Part of calculator project functionality except the AC key

---
+++ index.js
@@ -0,0 +1,86 @@
+const calculator = document.querySelector(".calculator");
+const keys = calculator.querySelector(".calculator__keys");
+const display = document.querySelector(".calculator__display");
+
+keys.addEventListener("click", (e) => {
+  if (e.target.matches("button")) {
+    // listen for all key presses &determine wich key was pressed
+    const key = e.target;
+
+    // Remove .is-depressed class from all keys
+
+    Array.from(key.parentNode.children).forEach((k) =>
+      k.classList.remove("is-depressed")
+    );
+    const action = key.dataset.action;
+    const previousKeyType = calculator.dataset.previousKeyType;
+
+    //When a user hits a number key the calculator should display the number that was hit a number & The current displayed number
+    const keyContent = key.textContent;
+
+    //If the previousKeyType is an operator, we want to replace the displayed number with clicked number
+    const displayedNum = display.textContent;
+
+    //If the key does not have a data-action attribute, it must be a number key
+    if (!action) {
+      if (displayedNum === "0" || previousKeyType === "operator") {
+        // If the calculator shows 0, we want to replace the calculator’s display with the clicked key
+        display.textContent = keyContent;
+      } else {
+        //If the calculator shows a non-zero number, we want to append the clicked key to the displayed number.
+        display.textContent = displayedNum + keyContent;
+      }
+      console.log("number key!");
+    }
+    // If the key has a data-action that is either add, subtract, multiply or divide, we know the key is an operator.
+    else if (
+      action === "add" ||
+      action === "subtract" ||
+      action === "multiply" ||
+      action === "divide"
+    ) {
+      console.log("operator key!");
+      //user hits an operator key, the operator should be highlighted so user knows the operator is active.
+      key.classList.add("is-depressed");
+      // Add custom attribute tell if the previous key is an operator key
+      calculator.dataset.previousKeyType = "operator";
+      calculator.dataset.firstValue = displayedNum;
+      calculator.dataset.operator = action;
+    }
+    //If the key’s data-action is decimal, we know the user clicked on the decimal key.
+    else if (action === "decimal") {
+      //a decimal should appear on the display. If user hits any number after hitting a decimal key, the number should be appended on the display as well.
+      console.log("decimal key!");
+      //if the key’s data-action is clear, we know the user clicked on the clear (the one that says AC) key
+      display.textContent = displayedNum + ".";
+    } else if (action === "clear") {
+      console.log("clear key!");
+    }
+    // If the key’s data-action is calculate, we know the user clicked on the equals key.
+    else if (action === "calculate") {
+      const firstValue = calculator.dataset.firstValue;
+      const operator = calculator.dataset.operator;
+      const secondValue = displayedNum;
+
+      display.textContent = calculate(firstValue, operator, secondValue);
+      console.log("equal key!");
+    }
+  }
+} );
+
+const calculate = (n1, operator, n2) => {
+    // Perform calculation and return calculated value
+     let result = "";
+
+     if (operator === "add") {
+       result = parseFloat(n1) + parseFloat(n2);
+     } else if (operator === "subtract") {
+       result = parseFloat(n1) - parseFloat(n2);
+     } else if (operator === "multiply") {
+       result = parseFloat(n1) * parseFloat(n2);
+     } else if (operator === "divide") {
+       result = parseFloat(n1) / parseFloat(n2);
+     }
+
+     return result;
+};
